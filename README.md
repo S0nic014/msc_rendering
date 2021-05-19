cf62c9c0d1601478b2ae5f39db7ad9a62f69d9ce
S0nic014 <47799401+S0nic014@users.noreply.github.com>
2021-05-19 17:23:40 +0100

    Add render config file

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # msc_rendering
  Msc Simulation and rendering; Renderman
+
+Requirements
+---
+- Python 3.5.2+
+- Renderman 23.5
--- msc_rendering/main.py
+++ msc_rendering/main.py
@@ -1,20 +1,28 @@
 import prman
 import pathlib
+import json
 from msc_rendering import Logger
 import msc_rendering.shaderFn as shaderFn
-import msc_rendering.primitivesFn as primitivesFn
 import msc_rendering.yakult as yakult
 import msc_rendering.table as table
 
-
+CONFIG_PATH = pathlib.Path.cwd() / 'msc_rendering' / 'render_config.json'
 ARCHIVES_DIR = pathlib.Path.cwd() / 'msc_rendering' / 'archives'
 SHADERS_DIR = pathlib.Path.cwd() / 'msc_rendering' / 'shaders'
 TEXTURES_DIR = pathlib.Path.cwd() / 'msc_rendering' / 'textures'
 
 
-def main(recompile_shaders=True, resolution=(720, 576), render_to_file: bool = False, hdri="cafe"):
+def load_config() -> dict:
+    config_dict = {}
+    with CONFIG_PATH.open() as jsonfile:
+        config_dict = json.load(jsonfile)
+    return config_dict
+
+
+def main():
+    config = load_config()
     shaderFn.tx_textures(TEXTURES_DIR)
-    if recompile_shaders:
+    if config.get('recompile_shaders', True):
         shaderFn.compile_shaders(shaders_paths=shaderFn.list_shader_files(SHADERS_DIR))
 
     # Interface
@@ -29,22 +37,22 @@ def main(recompile_shaders=True, resolution=(720, 576), render_to_file: bool = F
     ri.Option('searchpath', {'string texture': TEXTURES_DIR.as_posix()})
 
     # Display
-    file_name = 'yakult.exr'
-    if render_to_file:
+    file_name = config.get('output_file', 'yakult.exr')
+    if config.get('file_render', False):
         output = 'openexr'
     else:
         output = 'it'
 
     ri.Display(file_name, output, "rgba")
-    ri.Format(*resolution, 1)
+    ri.Format(*config.get('resolution', (720, 576)), 1)
 
     # Raytrace /integrators
     ri.DepthOfField(20, 1.5, 2.5)
-    ri.ShadingRate(10)
+    ri.ShadingRate(config.get('shading_rate', 10))
     ri.Hider("raytrace", {"int incremental": [1]})
-    ri.PixelVariance(0.01)
+    ri.PixelVariance(config.get('pixel_variance', 0.01))
     ri.Integrator("PxrPathTracer", "integrator")
-    ri.Projection(ri.PERSPECTIVE, {ri.FOV: 90})
+    ri.Projection(ri.PERSPECTIVE, {ri.FOV: config.get('fov', 90)})
 
     # World descrition
     ri.WorldBegin()
@@ -58,11 +66,12 @@ def main(recompile_shaders=True, resolution=(720, 576), render_to_file: bool = F
     ri.Rotate(-90, 1, 0, 0)
 
     # Cafe
+    hdri = config.get('hdri', 'cafe')
     if hdri == "cafe":
         ri.Rotate(-40, 0, 0, 1)
         ri.Light('PxrDomeLight', 'skyDome', {'float exposure': [0],
                                              'float intensity': [0.8],
-                                             "string lightColorMap": "comfy_cafe_2k.tx"})
+                                             "string lightColorMap": "cafe.tx"})
     elif hdri == "kitchen":
         ri.Rotate(120, 0, 0, 1)
         ri.Light('PxrDomeLight', 'skyDome', {'float exposure': [0],
@@ -104,6 +113,4 @@ def main(recompile_shaders=True, resolution=(720, 576), render_to_file: bool = F
 
 
 if __name__ == "__main__":
-    # main(recompile_shaders=True, resolution=(720, 576))
-    main(recompile_shaders=True, resolution=(1920, 1080), render_to_file=False)
-    # main(recompile_shaders=True, resolution=(3840, 2160), render_to_file=True)
+    main()
